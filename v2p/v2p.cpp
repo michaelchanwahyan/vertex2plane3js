accc1e57636bc090e4a9268f9cc635e4a560674f
michelson <michaelchan_wahyan@yahoo.com.hk>
2019-01-24 17:10:18 +0800

    [update] add division by zero handle

--- v2p/v2p.cpp
+++ v2p/v2p.cpp
@@ -18,40 +18,40 @@
 
 // trim from start (in place)
 void ltrim(std::string &s) {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
-		return !std::isspace(ch);
-	}));
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
 }
 
 // trim from end (in place)
 void rtrim(std::string &s) {
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
-		return !std::isspace(ch);
-	}).base(), s.end());
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
 }
 
 // trim from both ends (in place)
 void trim(std::string &s) {
-	ltrim(s);
-	rtrim(s);
+    ltrim(s);
+    rtrim(s);
 }
 
 // trim from start (copying)
 std::string ltrim_copy(std::string s) {
-	ltrim(s);
-	return s;
+    ltrim(s);
+    return s;
 }
 
 // trim from end (copying)
 std::string rtrim_copy(std::string s) {
-	rtrim(s);
-	return s;
+    rtrim(s);
+    return s;
 }
 
 // trim from both ends (copying)
 std::string trim_copy(std::string s) {
-	trim(s);
-	return s;
+    trim(s);
+    return s;
 }
 
 // ============================
@@ -60,204 +60,212 @@ std::string trim_copy(std::string s) {
 
 void line_split(const std::string &inputstring, std::vector<std::string> &output, char delim)
 {
-	size_t pos = inputstring.find(delim);
-	size_t initialPos = 0;
-	output.clear();
+    size_t pos = inputstring.find(delim);
+    size_t initialPos = 0;
+    output.clear();
 
-	// Decompose statement
-	while (pos != std::string::npos) {
-		output.push_back(inputstring.substr(initialPos, pos - initialPos));
-		initialPos = pos + 1;
+    // Decompose statement
+    while (pos != std::string::npos) {
+        output.push_back(inputstring.substr(initialPos, pos - initialPos));
+        initialPos = pos + 1;
 
-		pos = inputstring.find(delim, initialPos);
-	}
+        pos = inputstring.find(delim, initialPos);
+    }
 
-	// Add the last one
-	output.push_back(inputstring.substr(initialPos, std::min(pos, inputstring.size()) - initialPos + 1));
+    // Add the last one
+    output.push_back(inputstring.substr(initialPos, std::min(pos, inputstring.size()) - initialPos + 1));
 
-	return;
+    return;
 }
 
 std::vector<int> argSortVertexCoord(const std::vector<std::vector<float>>& vertex, const int axisIdx) {
-	// axisIdx: == 0 -> using x axis to sort
-	//          == 1 -> using y axis to sort
-	//          == 2 -> using z axis to sort
-	size_t Len = vertex.size();
-	std::vector<int> idx(Len, 0);
-	for (int i = 0; i < Len; i++) {
-		idx[i] = i;
-	}
-	//std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2){return vertex[i1] < vertex[i2];});
-	if (axisIdx == 0) {
-		std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2) {return vertex[i1][0] < vertex[i2][0]; });
-	}
-	if (axisIdx == 1) {
-	    std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2) {return vertex[i1][1] < vertex[i2][1]; });
+    // axisIdx: == 0 -> using x axis to sort
+    //          == 1 -> using y axis to sort
+    //          == 2 -> using z axis to sort
+    size_t Len = vertex.size();
+    std::vector<int> idx(Len, 0);
+    for (int i = 0; i < Len; i++) {
+        idx[i] = i;
     }
-	if (axisIdx == 2) {
-		std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2) {return vertex[i1][2] < vertex[i2][2]; });
-	}
-	return idx;
+    //std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2){return vertex[i1] < vertex[i2];});
+    if (axisIdx == 0) {
+        std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2) {return vertex[i1][0] < vertex[i2][0]; });
+    }
+    if (axisIdx == 1) {
+        std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2) {return vertex[i1][1] < vertex[i2][1]; });
+    }
+    if (axisIdx == 2) {
+        std::sort(idx.begin(), idx.end(), [&vertex](int i1, int i2) {return vertex[i1][2] < vertex[i2][2]; });
+    }
+    return idx;
 }
 
 int main()
 {
-	//std::string inputfilename = "vertices.txt";
-	std::string inputfilename;
-	std::vector<std::string> values;
-	std::vector<std::vector<float>> vertex_xyz;
-	std::string line = "";
-	std::ifstream fin;
-	std::ofstream fout;
-	int vertexNum = 4;
-	int wallIdx = 0;
-	char delim = '\t';
+    //std::string inputfilename = "vertices.txt";
+    std::string inputfilename;
+    std::vector<std::string> values;
+    std::vector<std::vector<float>> vertex_xyz;
+    std::string line = "";
+    std::ifstream fin;
+    std::ofstream fout;
+    int vertexNum = 4;
+    int wallIdx = 0;
+    char delim = '\t';
 
-	// input stage
-	//std::cout << "input file name: " << std::endl;
-	//std::cin >> inputfilename;
-	//inputfilename = "wallvertex_0_0.txt";
-	//std::cout << "name the wall index: " << std::endl;
-	//std::cin >> wallIdx;
+    // input stage
+    //std::cout << "input file name: " << std::endl;
+    //std::cin >> inputfilename;
+    //inputfilename = "wallvertex_0_0.txt";
+    //std::cout << "name the wall index: " << std::endl;
+    //std::cin >> wallIdx;
 
-	fout.open("temp_dump.txt", std::ios::out);
+    fout.open("temp_dump.txt", std::ios::out);
 
-	for (int wallIdx = 0; wallIdx < 5; wallIdx++) {
-		inputfilename = "wallvertex_0_";
-		inputfilename.append(std::to_string(wallIdx)).append(".txt");
-		std::cout << inputfilename << std::endl;
-		vertex_xyz.resize(4);
-		std::cout << "reading 4 vertices" << std::endl;
+    for (int wallIdx = 0; wallIdx < 5; wallIdx++) {
+        inputfilename = "wallvertex_0_";
+        inputfilename.append(std::to_string(wallIdx)).append(".txt");
+        std::cout << inputfilename << std::endl;
+        vertex_xyz.resize(4);
+        std::cout << "reading 4 vertices" << std::endl;
 
-		fin.open(inputfilename, std::ios::in);
-		for (int i = 0; i < vertexNum; i++) {
-			getline(fin, line);
-			vertex_xyz[i].resize(3);
-			line_split(line, values, delim);
-			for (int j = 0; j < 3; j++) {
-				vertex_xyz[i][j] = std::stof(trim_copy(values[j])) / 100;
-				std::cout << vertex_xyz[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		fin.close();
+        fin.open(inputfilename, std::ios::in);
+        for (int i = 0; i < vertexNum; i++) {
+            getline(fin, line);
+            vertex_xyz[i].resize(3);
+            line_split(line, values, delim);
+            for (int j = 0; j < 3; j++) {
+                vertex_xyz[i][j] = std::stof(trim_copy(values[j])) / 100;
+                std::cout << vertex_xyz[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        fin.close();
 
-		// parameter generation stage
-		// ---------------------------
-		// width and height
-		float _x_diff, _y_diff, _z_diff;
-		float _3js_wall_width, _3js_wall_height = 0.0f;
-		std::vector<int> flat_plane_vertex_sort_order;
-		_z_diff = vertex_xyz[2][2] - vertex_xyz[0][2];
+        // parameter generation stage
+        // ---------------------------
+        // width and height
+        float _x_diff, _y_diff, _z_diff;
+        float _3js_wall_width, _3js_wall_height = 0.0f;
+        std::vector<int> flat_plane_vertex_sort_order;
+        _z_diff = vertex_xyz[2][2] - vertex_xyz[0][2];
 
-		if (_z_diff > 10)
-		{
-			_x_diff = vertex_xyz[0][0] - vertex_xyz[1][0];
-			_y_diff = vertex_xyz[0][1] - vertex_xyz[1][1];
-			_3js_wall_width = std::sqrt(_x_diff * _x_diff + _y_diff * _y_diff);
-			_3js_wall_height = std::abs(_z_diff);
-		}
-		else // the case where the wall is actualy ceiling or ground
-		{
-			// detect the points for which the points are most negative in y-axis value
-			flat_plane_vertex_sort_order = argSortVertexCoord(vertex_xyz, 1);
-			// take the length of line joint by the two points
-			// with most negative y coordinates as the width
-			_x_diff = vertex_xyz[flat_plane_vertex_sort_order[0]][0] - vertex_xyz[flat_plane_vertex_sort_order[1]][0];
-			_y_diff = vertex_xyz[flat_plane_vertex_sort_order[0]][1] - vertex_xyz[flat_plane_vertex_sort_order[1]][1];
-			_3js_wall_width = std::sqrt(_x_diff * _x_diff + _y_diff * _y_diff);
-			// take the length of line joint by the 2nd and 3rd points
-			// with most negative y coordinates as the height
-			_x_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][0] - vertex_xyz[flat_plane_vertex_sort_order[2]][0];
-			_y_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][1] - vertex_xyz[flat_plane_vertex_sort_order[2]][1];
-			_3js_wall_height = std::sqrt(_x_diff * _x_diff + _y_diff * _y_diff);
-		}
+        if (_z_diff > 10)
+        {
+            _x_diff = vertex_xyz[0][0] - vertex_xyz[1][0];
+            _y_diff = vertex_xyz[0][1] - vertex_xyz[1][1];
+            _3js_wall_width = std::sqrt(_x_diff * _x_diff + _y_diff * _y_diff);
+            _3js_wall_height = std::abs(_z_diff);
+        }
+        else // the case where the wall is actualy ceiling or ground
+        {
+            // detect the points for which the points are most negative in y-axis value
+            flat_plane_vertex_sort_order = argSortVertexCoord(vertex_xyz, 1);
+            // take the length of line joint by the two points
+            // with most negative y coordinates as the width
+            _x_diff = vertex_xyz[flat_plane_vertex_sort_order[0]][0] - vertex_xyz[flat_plane_vertex_sort_order[1]][0];
+            _y_diff = vertex_xyz[flat_plane_vertex_sort_order[0]][1] - vertex_xyz[flat_plane_vertex_sort_order[1]][1];
+            _3js_wall_width = std::sqrt(_x_diff * _x_diff + _y_diff * _y_diff);
+            // take the length of line joint by the 2nd and 3rd points
+            // with most negative y coordinates as the height
+            _x_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][0] - vertex_xyz[flat_plane_vertex_sort_order[2]][0];
+            _y_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][1] - vertex_xyz[flat_plane_vertex_sort_order[2]][1];
+            _3js_wall_height = std::sqrt(_x_diff * _x_diff + _y_diff * _y_diff);
+        }
 
-		std::cout << std::endl << "width and height: " << _3js_wall_width << ", " << _3js_wall_height << std::endl;
+        std::cout << std::endl << "width and height: " << _3js_wall_width << ", " << _3js_wall_height << std::endl;
 
-		// ---------------------------
-		// x y z offset
-		Eigen::MatrixXf A(4, 3);
-		Eigen::VectorXf b(4);
-		A << vertex_xyz[0][0], vertex_xyz[0][1], vertex_xyz[0][2],
-			vertex_xyz[1][0], vertex_xyz[1][1], vertex_xyz[1][2],
-			vertex_xyz[2][0], vertex_xyz[2][1], vertex_xyz[2][2],
-			vertex_xyz[3][0], vertex_xyz[3][1], vertex_xyz[3][2];
-		b << -1, -1, -1, -1;
-		Eigen::Vector3f v = A.colPivHouseholderQr().solve(b); // the v here solves for a, b, and c, while d is hardcoded as 1
-					// std::cout << "The solution is:\n" << v << std::endl;
-					// 2.b) distance of wall plane from origin
-		float _3js_wall_distance_from_origin = 1.0f / v.norm();
-		std::cout << std::endl << "distance of plane from origin: " << _3js_wall_distance_from_origin << std::endl;
-		float u[3] = { v[0] / v.norm() , v[1] / v.norm() , v[2] / v.norm() };
+        // ---------------------------
+        // x y z offset
+        Eigen::MatrixXf A(4, 3);
+        Eigen::VectorXf b(4);
+        A << vertex_xyz[0][0], vertex_xyz[0][1], vertex_xyz[0][2],
+            vertex_xyz[1][0], vertex_xyz[1][1], vertex_xyz[1][2],
+            vertex_xyz[2][0], vertex_xyz[2][1], vertex_xyz[2][2],
+            vertex_xyz[3][0], vertex_xyz[3][1], vertex_xyz[3][2];
+        b << -1, -1, -1, -1;
+        Eigen::Vector3f v = A.colPivHouseholderQr().solve(b); // the v here solves for a, b, and c, while d is hardcoded as 1
+                    // std::cout << "The solution is:\n" << v << std::endl;
+                    // 2.b) distance of wall plane from origin
+        float _3js_wall_distance_from_origin = 1.0f / v.norm();
+        std::cout << std::endl << "distance of plane from origin: " << _3js_wall_distance_from_origin << std::endl;
+        float u[3] = { v[0] / v.norm() , v[1] / v.norm() , v[2] / v.norm() };
 
-		std::vector<float> check_pt_xyz;
-		check_pt_xyz.resize(3);
-		check_pt_xyz[0] = _3js_wall_distance_from_origin * u[0];
-		check_pt_xyz[1] = _3js_wall_distance_from_origin * u[1];
-		check_pt_xyz[2] = _3js_wall_distance_from_origin * u[2];
+        std::vector<float> check_pt_xyz;
+        check_pt_xyz.resize(3);
+        check_pt_xyz[0] = _3js_wall_distance_from_origin * u[0];
+        check_pt_xyz[1] = _3js_wall_distance_from_origin * u[1];
+        check_pt_xyz[2] = _3js_wall_distance_from_origin * u[2];
 
-		float normal_vector_direction_checkValue = v[0] * check_pt_xyz[0] + v[1] * check_pt_xyz[1] + v[2] * check_pt_xyz[2] + 1;
-		if (normal_vector_direction_checkValue * normal_vector_direction_checkValue < 0.001) {
-			std::cout << std::endl << "normal does not need to toggle direction. it is pointing outward from origin" << std::endl;
-		}
-		else {
-			std::cout << std::endl << "normal needs to toggle direction in order to be pointing outward from origin" << std::endl;
-			v[0] = -v[0];
-			v[1] = -v[1];
-			v[2] = -v[2];
-			u[0] = v[0] / v.norm();
-			u[1] = v[1] / v.norm();
-			u[2] = v[2] / v.norm();
-		}
-		std::cout << "normal vector is " << std::endl << v << std::endl;
-		//float u[3] = { v[0] / v.norm() , v[1] / v.norm() , v[2] / v.norm() };
-		std::cout << "normalized normal vector is " << std::endl << u[0] << ", " << u[1] << ", " << u[2] << std::endl;
+        float normal_vector_direction_checkValue = v[0] * check_pt_xyz[0] + v[1] * check_pt_xyz[1] + v[2] * check_pt_xyz[2] + 1;
+        if (normal_vector_direction_checkValue * normal_vector_direction_checkValue < 0.001) {
+            std::cout << std::endl << "normal does not need to toggle direction. it is pointing outward from origin" << std::endl;
+        }
+        else {
+            std::cout << std::endl << "normal needs to toggle direction in order to be pointing outward from origin" << std::endl;
+            v[0] = -v[0];
+            v[1] = -v[1];
+            v[2] = -v[2];
+            u[0] = v[0] / v.norm();
+            u[1] = v[1] / v.norm();
+            u[2] = v[2] / v.norm();
+        }
+        std::cout << "normal vector is " << std::endl << v << std::endl;
+        //float u[3] = { v[0] / v.norm() , v[1] / v.norm() , v[2] / v.norm() };
+        std::cout << "normalized normal vector is " << std::endl << u[0] << ", " << u[1] << ", " << u[2] << std::endl;
 
-		float _js_offsetX, _js_offsetY, _js_offsetZ;
-		float _tempValue_x = 0.0f, _tempValue_y = 0.0f, _tempValue_z = 0.0f;
-		for (int i = 0; i < 4; i++) {
-			_tempValue_x = _tempValue_x + vertex_xyz[i][0];
-			_tempValue_y = _tempValue_y + vertex_xyz[i][1];
-			_tempValue_z = _tempValue_z + vertex_xyz[i][2];
-		}
-		_js_offsetX = _tempValue_x / 4; //_3js_wall_distance_from_origin * u[0];
-		_js_offsetY = _tempValue_z / 4; //_3js_wall_distance_from_origin * u[2];
-		_js_offsetZ = -_tempValue_y / 4; //_3js_wall_distance_from_origin * - u[1];
+        float _js_offsetX, _js_offsetY, _js_offsetZ;
+        float _tempValue_x = 0.0f, _tempValue_y = 0.0f, _tempValue_z = 0.0f;
+        for (int i = 0; i < 4; i++) {
+            _tempValue_x = _tempValue_x + vertex_xyz[i][0];
+            _tempValue_y = _tempValue_y + vertex_xyz[i][1];
+            _tempValue_z = _tempValue_z + vertex_xyz[i][2];
+        }
+        _js_offsetX = _tempValue_x / 4; //_3js_wall_distance_from_origin * u[0];
+        _js_offsetY = _tempValue_z / 4; //_3js_wall_distance_from_origin * u[2];
+        _js_offsetZ = -_tempValue_y / 4; //_3js_wall_distance_from_origin * - u[1];
 
-		std::cout << std::endl << "offset in 3js coord system:" << std::endl
-			<< _js_offsetX << ", " << _js_offsetY << ", " << _js_offsetZ << std::endl;
+        std::cout << std::endl << "offset in 3js coord system:" << std::endl
+            << _js_offsetX << ", " << _js_offsetY << ", " << _js_offsetZ << std::endl;
 
-		// ---------------------------
-		// rotation angle
-		float _js_rotateX = 0.0f, _js_rotateY = 0.0f, _js_rotateZ = 0.0f;
-		if (u[1] * u[1] < 0.001) {
-			if (u[2] > 0) {
-				_js_rotateX = 3.141592654f / 2.0f;
-			}
-			else {
-				_js_rotateX = -3.141592654f / 2.0f;
-			}
-		}
-		else {
-			_js_rotateX = std::atanf(u[2] / u[1]);
-		}
-		if (_z_diff > 10) {
-			_js_rotateY = std::atanf(u[1] / u[0]) - 3.141592654f / 2.0f;
-			_js_rotateZ = std::atanf(u[2] / u[0]);
-		}
-		else {
-			_js_rotateY = 0;
-			//_js_rotateZ = std::atanf(_y_diff / _x_diff) - 3.141592654f / 2.0f;
-			_x_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][0] - vertex_xyz[flat_plane_vertex_sort_order[0]][0];
-			_y_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][1] - vertex_xyz[flat_plane_vertex_sort_order[0]][1];
-			_js_rotateZ = std::atanf(_y_diff / _x_diff);
-		}
+        // ---------------------------
+        // rotation angle
+        float _js_rotateX = 0.0f, _js_rotateY = 0.0f, _js_rotateZ = 0.0f;
+        if (u[1] * u[1] > 0.001) {
+            _js_rotateX = std::atanf(u[2] / u[1]);
+        }
+        else {
+            _js_rotateX = u[2] > 0 ? 3.141592654f / 2.0f : -3.141592654f / 2.0f;
+        }
+        if (_z_diff > 10) {
+            //_js_rotateY = std::atanf(u[1] / u[0]) - 3.141592654f / 2.0f;
+            //_js_rotateZ = std::atanf(u[2] / u[0]);
+            if (u[0]*u[0] > 0.001) {
+                _js_rotateY = std::atanf(u[1] / u[0]) - 3.141592654f / 2.0f;
+            }
+            else {
+                _js_rotateY = u[1] > 0 ? 3.141592654f / 2.0f : -3.141592654.0f / 2.0f;
+                _js_rotateY = _js_rotateY - 3.141592654f / 2.0f;
+                _js_rotateZ = u[2] > 0 ? 3.141592654f / 2.0f : -3.141592654.0f / 2.0f;
+            }
+        }
+        else {
+            _js_rotateY = 0;
+            //_js_rotateZ = std::atanf(_y_diff / _x_diff) - 3.141592654f / 2.0f;
+            _x_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][0] - vertex_xyz[flat_plane_vertex_sort_order[0]][0];
+            _y_diff = vertex_xyz[flat_plane_vertex_sort_order[1]][1] - vertex_xyz[flat_plane_vertex_sort_order[0]][1];
+            if (_x_diff * _x_diff > 0.001) {
+                _js_rotateZ = std::atanf(_y_diff / _x_diff);
+            }
+            else {
+                _js_rotateZ = _y_diff > 0 ? 3.141592654f / 2.0f : -3.141592654f / 2.0f;
+            }
+        }
 
-		// ---------------------------
-		// dump 3js code
-		fout << "        "
-			<< "var planeGeometry_0" << wallIdx << " = new THREE.PlaneGeometry(" << _3js_wall_width << "," << _3js_wall_height << ");\n\
+        // ---------------------------
+        // dump 3js code
+        fout << "        "
+            << "var planeGeometry_0" << wallIdx << " = new THREE.PlaneGeometry(" << _3js_wall_width << "," << _3js_wall_height << ");\n\
         var planeMaterial_0" << wallIdx << " = new THREE.MeshStandardMaterial({ color:0xff0000 });\n\
         var plane_0" << wallIdx << " = new THREE.Mesh(planeGeometry_0" << wallIdx << ", planeMaterial_0" << wallIdx << ");\n\
         // we should perform translation first,\n\
@@ -273,10 +281,10 @@ int main()
         plane_0" << wallIdx << ".receiveShadow = true;\n\
         scene.add(plane_0" << wallIdx << ");\n";
 
-		fout << "\n";
-	}
-	fout.close();
-	std::getchar();
+        fout << "\n";
+    }
+    fout.close();
+    std::getchar();
 
-	return 0;
+    return 0;
 }
